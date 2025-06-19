6f7243f064e3ac3b1d3495f200743a42ab6bd343
Desternow <aleksander.romanov2024@gmail.com>
2025-06-19 19:00:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
+import os
+import sys
 from PyQt5 import QtWidgets
 from interface import MainWindow
+from config import output_pdf_path
 
 def main():
-    import sys
+    # Получаем директорию из пути
+    output_dir = os.path.dirname(output_pdf_path)
+
+    # Проверяем и создаём папку, если её нет
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+        print(f"Создана папка: {output_dir}")
+
+    # Запуск приложения
     app = QtWidgets.QApplication(sys.argv)
     window = MainWindow()
     window.show()
